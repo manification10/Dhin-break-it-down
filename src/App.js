b197ce50ee71d17c9ca9b572e1fdafe05b334749
Ananya Paul <ap2273@cornell.edu>
2020-03-19 21:46:46 -0700

    VideoPlayer added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import VideoApp from './VideoApp.js';
+import VideoPlayer from './VideoPlayer.js'
 
 function App() {
+  const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  loop: true,
+  playbackRates: [0.5, 1, 1.5, 2],
+  sources: [{
+    src: "http://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
+    type: 'video/mp4'
+  }]
+}
+
   return (
     <div className="App">
       <header className="App-header">
-      <VideoApp />
+      <VideoPlayer { ...videoJsOptions } />
       </header>
     </div>
   );
